82dca1d0839852eaa63b46a364826ff815402db4
besrour walid <walidbesrour@gmail.com>
2021-02-14 17:40:40 +0100

    intent_kotlin

---
+++ app/src/main/java/com/example/test_kotlin/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.test_kotlin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    val a = "ssss"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var txt = mytxt.text.toString()
+        new_btn.setOnClickListener {
+
+            val intent = Intent(this,MainActivity2::class.java).apply {
+                putExtra("wal",txt)
+                println(a)
+            }
+            startActivity(intent)
+        }
+    }
+}
